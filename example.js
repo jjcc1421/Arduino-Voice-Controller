25752e6e8d2facc4b1d8572bcfd84fb7acd6ab33
jcaicedo <jcaicedo@conversantmedia.com>
2018-08-08 19:10:50 -0500

    Added base example and npm scripts

---
+++ example.js
@@ -0,0 +1,13 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+
+  var led = new five.Led(13);
+
+  // "blink" the led in 500ms on-off phase periods
+  led.on();
+  setTimeout(()=>{
+    led.off();   
+  }, 5000)
+});
